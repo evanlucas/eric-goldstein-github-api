f3b7fec81e1ca04e9281101098f91c4d41ac6eec
Eric L. Goldstein <elg2001@columbia.edu>
2015-05-06 11:37:57 -0500

    Added database dump file to build the database necessary for the database caching to work

---
+++ database/dbdump.sql
@@ -0,0 +1,67 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: github_cache; Type: TABLE; Schema: public; Owner: eric; Tablespace: 
+--
+
+CREATE TABLE github_cache (
+    repo_owner character varying(255),
+    repo_name character varying(255),
+    ts timestamp without time zone,
+    data json
+);
+
+
+ALTER TABLE public.github_cache OWNER TO eric;
+
+--
+-- Data for Name: github_cache; Type: TABLE DATA; Schema: public; Owner: eric
+--
+
+COPY github_cache (repo_owner, repo_name, ts, data) FROM stdin;
+\.
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
